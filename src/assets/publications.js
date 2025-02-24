f883d4f183eb34b69bcf22a6f0b71dba4b7bfa81
Yuvalefr <zxrtde@gmail.com>
2025-02-24 12:02:39 -0500

    updated publications

--- src/assets/publications.js
+++ src/assets/publications.js
@@ -1,6 +1,28 @@
 const publications = [
   {
     authors:
+      "With Ertem Nusret Tas",
+    title:
+      "Dynamically Available Common Subset",
+    type: "conference",
+    paper_link:"https://eprint.iacr.org/2025/016",
+    abstract: `Internet-scale consensus protocols used by blockchains are designed
+     to remain operational in the presence of unexpected temporary crash faults (the so-called \emph{sleepy} model of consensus)
+      -- a critical feature for the latency-sensitive financial applications running on these systems. 
+
+      However, their leader-based architecture, where a single block proposer is responsible for creating the block at each height,
+      makes them vulnerable to short-term censorship attacks, in which the proposers profit at the application layer by excluding certain transactions. 
+      In this work, we introduce an atomic broadcast protocol, secure in the sleepy model,
+       that ensures the inclusion of all transactions within a constant expected time, 
+       provided that at least one participating node is non-censoring at all times. 
+      Unlike traditional approaches, our protocol avoids designating a single proposer per block height, 
+      instead leveraging a so-called dynamically available common subset (DACS) protocol -- the first of its kind in the sleepy model. 
+      Additionally, our construction guarantees deterministic synchronization -- once an honest node confirms a block, 
+      all other honest nodes do so within a constant time, thus addressing a shortcoming of many low-latency sleepy protocols.`,
+    venue: "In Submission",
+  },
+  {
+    authors:
       "With Joseph Bonneau, Benedikt Bunz, Miranda Christ",
     title:
       "How Much Public Randomness Do Modern Consensus Protocols Need?",
